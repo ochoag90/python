07a4b4a751337a8555d82f4903f3f2ab84f76988
Gabe <gochoa5190@gmail.com>
2016-09-12 21:30:17 -0400

    Add files via upload
    Input a number by a user, checks to see if the number you chose matches the number the computer chose at random. If it is the same it will ask you if you want to play again. If you guess more than 5 times, and you still have it wrong, it will end the game and ask you if you want to play again. If you guess it right, it will congratulate you. Also, this prints out the guesses you have already tried.

--- Number_Game.py
+++ Number_Game.py
@@ -1,20 +1,39 @@
 import random
 
-# Generate a random number between 1-10
-secret_num = random.randint(1, 10)
+def game():
 
-while True:
-    # Get a number guess from the player
-    guess = int(input("Guess a number between 1 and 10! "))
-    
-    # Compare guess to secret number
-    if guess == secret_num:
-        print("You got it! my number was {}".format(secret_num))
-        break
-    elif guess != secret_num:
-        print("That's not right!")
-    elif guess == "I GIVE UP":
-        print("YOU CANT GIVE UP!")
+    # Generate a random number between 1-10
+    secret_num = random.randint(1, 10)
+    guesses = []
+
+
+    while len(guesses) < 5:
+        try:
+            # Get a number guess from the player
+            guess = int(input("Guess a number between 1 and 10! "))
+        except ValueError:
+            print("{} isn't a number!".format(guess))
+        else:
+            # Compare guess to secret number
+            if guess == secret_num:
+                print("You got it! my number was {}".format(secret_num))
+                break
+            elif guess < secret_num:
+                print("My number is higher than {}".format(guess))
+            else:
+                print("The number is lower than {}!".format(guess))
+            guesses.append(guess)
+            print("These are your current guesses!" + str(guesses))
+
     else:
-        continue
-    # print hit/miss
+        print("You didn't get it! my number was {}!".format(secret_num))
+
+    play_again = input("Do you want to play again? y/n ")
+    if play_again == 'y':
+        game()
+    elif play_again == 'n':
+        print('bye')
+    #else:
+        #print("Bye!")
+game()
+
